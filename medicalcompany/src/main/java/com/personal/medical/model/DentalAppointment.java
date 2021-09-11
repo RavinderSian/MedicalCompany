8c05d72644fbff32968f45b4b2201576503c3e60
ravinder sian <rsian761@gmail.com>
2021-09-11 17:18:44 +0100

    service tests

--- medicalcompany/src/main/java/com/personal/medical/model/DentalAppointment.java
+++ medicalcompany/src/main/java/com/personal/medical/model/DentalAppointment.java
@@ -4,14 +4,21 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@Entity(name = "appointments")
+@Entity
 public class DentalAppointment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@Column(name = "description")
 	private String description;
---
+++ medicalcompany/src/test/java/com/personal/medical/services/DentalAppointmentServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.personal.medical.services;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.personal.medical.model.DentalAppointment;
+import com.personal.medical.repository.DentalAppointmentRepository;
+
+@SpringBootTest
+class DentalAppointmentServiceImplTest {
+
+	private DentalAppointmentService service;
+	
+	@Mock
+	private DentalAppointmentRepository repository;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		service = new DentalAppointmentServiceImpl(repository);
+	}
+
+	@Test
+	void test_ServiceNotNull() {
+		assertThat(service, not(equalTo(null)));
+	}
+	
+	@Test
+	void test_FindById_ReturnsEmptyOptional_WhenNoId_Present() {
+		
+		Optional<DentalAppointment> result = Optional.empty();
+		assertThat(result, equalTo(Optional.empty()));
+	}
+
+}
