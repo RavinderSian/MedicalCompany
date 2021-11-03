8049b3af82752d8e38b764e3dc84cdb31725887b
ravinder sian <rsian761@gmail.com>
2021-11-03 20:30:52 +0000

    add appointments project

---
+++ medicalcompany-appointments/src/main/java/com/personal/medical/appointments/MedicalcompanyAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.medical.appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalcompanyAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalcompanyAppointmentsApplication.class, args);
+	}
+
+}
